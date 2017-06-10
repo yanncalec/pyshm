09598c3ea82fd6c6574445a2a21e678568352d10
Han Wang <han.wang.fr@gmail.com>
2017-06-10 10:45:00 +0200

    RAS

---
+++ pyshm/script_remote/Download_data.py
@@ -0,0 +1,227 @@
+#!/usr/bin/env python
+
+"""Download or update the local database.
+
+Note:
+    The projects in the local database are organized by their PID (project key ID) of 3 digits. For example, the project 24 is saved in the folder '024'.
+"""
+
+import os, sys, argparse
+from pyshm.script import examplestyle, warningstyle
+
+class Options:
+    PID=None  # Key ID of the project to be downloaded, a number
+    force=False  # Force assembling of data
+    delete=False  # Delete failed projects (interactive)
+    endtime=None  # fetch data til this ending time
+    verbose=False  # print message
+
+
+def Download_data(dbdir, options):
+    """Download or update the local database from the server of OSMOS.
+
+    Args:
+        dbdir (string): the directory of the whole database
+        options: object including all options, e.g., returned by parser.parse_args()
+    """
+
+    import requests, json, pandas, colorama, dateutil
+    from pyshm import OSMOS
+
+    # disable certificate check
+    requests.packages.urllib3.disable_warnings()
+
+    # open the session
+    session = requests.session()
+
+    # get the token
+    payload = {"token":"null","action":"loginClient","data":{"email":options.login,"password":options.password}}
+    r = session.post("https://client.osmos-group.com/server/application.php", data=json.dumps(payload))
+    response = json.loads(r.text)
+    assert(response['result'] == 'SUCCESS')  # check the response from the server
+    # print(session.cookies)
+    # print(response)
+    # sys.exit()
+
+    # token to use for next requests
+    token = response['data']["tokenStore"]
+
+    # get informations of projects
+    payload = {"token": token, "action": "getlistprojectstoanalyse"}
+    r = session.post("https://client.osmos-group.com/server/application.php", data=json.dumps(payload))
+    response = json.loads(r.text)
+    assert(response['result'] == 'SUCCESS')  # check the response from the server
+    # print(response)
+    # sys.exit()
+
+    ListProject = response['data']  # List of projects
+
+    # Save in a dictionary with project keyid as keys
+    DicProject = {p['projectkeyid']:p for p in ListProject}
+    # which is the pythonic way for the following:
+    # DicProject = {}
+    # for p in ListProject:
+    #     DicProject[p['projectkeyid']] = p
+
+    # Informations of the available projects
+    if options.info:
+        toto = pandas.DataFrame(ListProject)
+        Pinfo = toto[['projectkeyid', 'name', 'level', 'start', 'end']].sort_values('projectkeyid').reset_index(drop=True)  # Information of projects
+        fname_info = os.path.join(dbdir, 'info.xlsx')
+        Pinfo.to_excel(fname_info)  # save (overwrite) the information in a Excel file
+        print('List of available projects saved in {}'.format(fname_info))
+        raise SystemExit
+
+    # List of PID to be updated
+    if options.PID is not None:  # PID given
+        if options.PID in DicProject.keys():  # check that the given PID is valid
+            ListPID = [options.PID]
+        else:
+            raise KeyError('PID {} not found'.format(options.PID))
+    else:  # PID not given
+        ListPID = list(DicProject.keys())  # use the whole list of PIDs
+        ListPID.sort()
+
+    # Download data from OSMOS's server
+    newdata_list = []  # PIDs with successful fetching of new data
+    failedPID_list = []  # PID with failed response from server or empty project
+
+    for pid in ListPID:  # iteration on the PID
+        projdir = os.path.join(dbdir, str(pid)) # output directory
+
+        info = DicProject[pid]
+        if options.verbose:
+            print('\n---------Fetching project {}---------'.format(pid))
+            print('Name: {}'.format(info['name']))
+            print('Level: {}'.format(info['level']))
+            print('Start: {}'.format(info['start']))
+            print('End: {}'.format(info['end']))
+            # # in case of error, try utf-8 encoding:
+            # print('Name: {}'.format(info['name']).encode('utf-8'))
+            # print('Level: {}'.format(info['level']).encode('utf-8'))
+            # print('Start: {}'.format(info['start']).encode('utf-8'))
+            # print('End: {}'.format(info['end']).encode('utf-8'))
+
+        try:
+            # new data are saved in sub-folders named by the locations' keyid
+            nflag, Liris = OSMOS.update_LIRIS_data_by_project(token, session, pid, projdir, endtime=options.endtime, verbose=options.verbose)
+
+            if nflag:  # successfully fetched new data
+                newdata_list.append(pid)
+            if nflag or options.force:
+                    # update the assembled pandas data file
+                    OSMOS.assemble_to_pandas(projdir)  # assemble to Raw.pkl in the project folder
+
+            # Save the information of the project in a text file
+            fname = os.path.join(projdir, 'info.txt') # output file name
+            if not os.path.isfile(fname):  # creat only if file does not exist
+                with open(fname, 'w') as fp:
+                    fp.writelines('---------------------------\n')
+                    fp.writelines('Information on the project:\n')
+                    fp.writelines('---------------------------\n')
+                    fp.writelines('name : {}\n'.format(info['name']))
+                    fp.writelines('level : {}\n'.format(info['level']))
+                    fp.writelines('start : {}\n'.format(info['start']))
+                    fp.writelines('end : {}\n'.format(info['end']))
+                    fp.writelines('---------------------------\n')
+                    fp.writelines('Information on the sensors:\n')
+                    fp.writelines('---------------------------\n')
+                    for liris in Liris:
+                        msg = 'Location ID: {}, Location name: {}, LIRIS ID: {}, LIRIS name:{}\n'.format(liris.locationkeyid, liris.locationname, liris.liriskeyid, liris.name)
+                        fp.writelines(msg)
+
+        except Exception as msg:
+            failedPID_list.append(pid)
+            print(colorama.Fore.RED + 'Error: ', msg)
+            print(colorama.Style.RESET_ALL)
+            # raise Exception
+
+    if options.verbose:
+        print('\nProject(s) with new data:', newdata_list)
+        print('Failed project(s):', failedPID_list)
+
+    if options.delete:
+        from shutil import rmtree  # high-level file operations
+
+        try:
+            for pid in failedPID_list:
+                projdir = os.path.join(dbdir, str(pid))
+                if os.path.isdir(projdir): # if the project folder exists
+                    ans = None
+                    while ans not in ['Y', 'N', '']:
+                        ans = input('Delete the folder of the project {} and all its contents [y/N]?'.format(pid)).capitalize()
+                    if ans=='Y':
+                        try:
+                            rmtree(projdir)
+                        except Exception as msg:
+                            print(msg)
+                            pass
+        except KeyboardInterrupt as e:
+            print()
+            pass
+
+    return newdata_list, failedPID_list
+
+
+__all__ = ['Download_data', 'Options']
+
+__script__ = __doc__
+
+__warning__ = "Warning:" + warningstyle("\n It is advised AGAINST manual modifications of the local database (e.g., insert, delete or rename files or folders in the database directory) which may lead to its dysfunction.")
+
+examples = []
+examples.append(["%(prog)s --info DBDIR", "Save the list of available projects into a file named info.xlsx in the local database directory DBDIR and exit."])
+examples.append(["%(prog)s -p 24 -v DBDIR", "Download or update the project of PID 24 in the directory DBDIR (this will create a project subfolder named 024 under DBDIR and generate a file named Raw.pkl) and print messages."])
+examples.append(["%(prog)s -v DBDIR", "Download or update all available projects in the directory DBDIR."])
+__example__ = "Some examples of use (change the path seperator '/' to '\\' on Windows platform):" + "".join([examplestyle(x) for x in examples])
+
+
+def main():
+    # usage_msg = '%(prog)s [options] <dbdir>'
+    # parser = argparse.ArgumentParser(description=__script__, usage=usage_msg)
+    parser = argparse.ArgumentParser(description=__script__,
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     epilog=__warning__ + "\n\n" + __example__)
+
+    parser.add_argument('dbdir', help='directory of the local OSMOS database')
+    parser.add_argument('-p', '--PID', dest='PID', type=str, default=None, help='project Key ID to be processed (by default all projects presented on the remote server will be processed)', metavar='integer')
+    parser.add_argument('--login', dest='login', type=str, default='be@osmos-group.com', help="login of Osmos server (default='be@osmos-group.com')", metavar='string')
+    parser.add_argument('--pw', dest='password', type=str, default='osmos', help="password of Osmos server (default='osmos')", metavar='string')
+
+    parser.add_argument('-e', '--end', dest='endtime', type=str, default=None, help='fetch data til this time (by default all data til today will be fetched)', metavar='YYYY-MM-DD')
+    # parser.add_option('-a', '--assemble', dest='assemble', action='store_true', default=False, help='Assemble all pkl files of different Liris of the same PID into a single pkl file named \'Raw_latest.pkl\'.')
+    parser.add_argument('-f', '--force', dest='force', action='store_true', default=False, help='force to assembling data of all sensors into a single file (even when no new data are fetched)')
+    parser.add_argument('-d', '--delete', dest='delete', action='store_true', default=False, help='delete failed projects from the local database')
+    parser.add_argument('--info', dest='info', action='store_true', default=False, help='save the list of available projects and exit')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='count', default=0, help='print messages')
+
+    options = parser.parse_args()
+
+    if options.PID is not None:  # string -> integer conversion
+        try:
+            options.PID = int(options.PID)
+        except:
+            raise ValueError("Invalid format of PID: {}".format(options.PID))
+
+    # if len(args) < 1:
+    #     print('Usage: '+usage_msg)
+    #     print(parm_msg)
+    #     sys.exit(0)
+    # else:  # check dbdir
+    #     dbdir = args[0]
+
+    # check the database directory
+    if not os.path.isdir(options.dbdir):
+        raise FileNotFoundError(options.dbdir)
+        # # create the directory if not existed
+        # try:
+        #     os.makedirs(options.dbdir)
+        # except OSError:
+        #     pass
+
+    # core function
+    Download_data(options.dbdir, options)
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyshm/script_remote/__init__.py
@@ -0,0 +1,200 @@
+from functools import wraps
+import json, numpy, pickle, pandas
+import colorama
+import numpy as np
+import os
+
+# Some console text formation styles
+warningstyle = lambda x: colorama.Style.BRIGHT + colorama.Fore.RED + x + colorama.Style.RESET_ALL
+brightstyle = lambda x: colorama.Style.BRIGHT + x + colorama.Style.RESET_ALL
+examplestyle = lambda x: "\n  " + brightstyle(x[0]) + "\n\t" + x[1]
+
+
+class MyEncoder(json.JSONEncoder):
+    """Utility class for convert from numpy types to json.
+
+    """
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        # elif isinstance(obj, numpy.bool_):
+        #     return bool(obj)
+        else:
+            return super(MyEncoder, self).default(obj)
+
+
+def to_json(X, verbose=False):
+    """Filter and convert a dictionary to json format.
+
+    A key-value pair in the dictionary will be convert to json if the value is one of the following types:
+        - pandas DataFrame or Series
+        - a numpy array
+        - a dictionary of numpy array
+        - None type
+    otherwise it will be filtered out.
+
+    Args:
+        X (dict): input dictionary
+    Returns:
+        a dictionary composed of the converted key-value pairs.
+    """
+    import numbers
+
+    Res = {}
+    for k, v in X.items():
+        if isinstance(v, pandas.DataFrame) or isinstance(v, pandas.Series):
+            Res[k] = v.to_json(date_unit='s', date_format='iso')
+        else:
+            try:
+                # if the value can be converted to json or if it is None, keep
+                # as it is json will not dump dictionary unless all keys are
+                # string.
+                if isinstance(v, dict):
+                    u = {str(k):v for k,v in v.items()}
+                    json.dumps(u, cls=MyEncoder)
+                    Res[k] = u
+                elif isinstance(v, np.ndarray) or isinstance(v, str) or isinstance(v, numbers.Number) or v is None:
+                    Res[k] = v
+                else:
+                    # This should not happen:
+                    raise TypeError('to_json: The values of the input dictionary is not valid.')
+            except Exception as msg:
+                if verbose:
+                    print(warningstyle("Warning:\n{}".format(msg)))
+                pass
+    return Res
+
+
+def load_result_of_analysis(fname):
+    """Utility function for loading pickle or json file saved by the scripts of data analysis.
+
+    Args:
+        fname (str): input file name
+    Return:
+        Res: a dictionary containing the contents of the input file
+    """
+    idx = fname.rfind('.')
+    if fname[idx+1:].lower() == 'pkl':
+        with open(fname, 'rb') as fp:
+            Res = pickle.load(fp)
+        return Res
+    elif fname[idx+1:].lower() == 'json':
+        with open(fname, 'r') as fp:
+            toto = json.load(fp)
+        Res = {}
+        for k,v in toto.items():
+            try:
+                Res[k] = pandas.read_json(v)
+            except Exception:
+                Res[k] = v
+        return Res
+    else:
+        raise TypeError('Unknown file type.')
+
+
+def per_sensor_result(X, Locations, aflag=True):
+    """Reorganize the result by sensor.
+
+    If aflag==True all non-pandas fields present in X will be preserved.
+    """
+    import pandas
+
+    Res = {}
+    for loc in Locations:
+        toto = {}
+        for k, v in X.items():
+            if isinstance(v, pandas.DataFrame) or isinstance(v, pandas.Series):
+                toto[k] = v[loc]
+        Res[loc] = pandas.DataFrame(toto)
+    if aflag:
+        for k, v in X.items():
+            if not (isinstance(v, pandas.DataFrame) or isinstance(v, pandas.Series)):
+                Res.update({k:v})
+
+    return Res
+
+raw2mm = lambda p, x: (p[0]*x**2+p[1]*x+p[2])/1000.-2.
+
+def static_data_analysis_template(func):
+    """Template for static data analysis algorithms.
+
+    This functional prepares data for the core algorithm and saves the
+    results in a dictionary. The core algorithm is passed by 'func' which
+    must be callable via the interface
+
+        resdic = func(options, Xcpn, Ycpn, *args, **kwargs)
+
+    the returned dictionary will be auguemented by some extra informations.
+
+    """
+    from pyshm import OSMOS
+    # import os
+    # import pickle
+    # import sys
+    # import json
+    # import numpy as np
+    # import pandas as pd
+    # import colorama
+    # import inspect
+
+    @wraps(func)
+    def newfunc(infile, outfile0, options, *args, **kwargs):
+
+        if options.component.upper() in ['SEASONAL', 'TREND']:
+            if options.verbose:
+                print('Decomposition of signals...')
+                print('\tMoving window estimator: {}\n\tMoving window size: {}\n\tOrder of the KZ filter: {}'.format(options.mwmethod, options.mwsize, options.kzord))
+
+            (Xall,Xsnl,Xtrd), (Yall,Ysnl,Ytrd), Midx = OSMOS.prepare_static_data(infile, mwsize=options.mwsize, kzord=options.kzord, method=options.mwmethod, timerange=(options.time0, options.time1))
+
+            if options.component.upper() == 'SEASONAL':
+                Xcpn, Ycpn = Xsnl, Ysnl
+            else:
+                Xcpn, Ycpn = Xtrd, Ytrd
+        elif options.component.upper() == 'ALL':
+            Xcpn, Ycpn, Midx = OSMOS.truncate_static_data(infile, (options.time0, options.time1))
+        else:
+            raise TypeError("{}: Wrong type of component.".format(options.component))
+
+        # Call the core function and save results in a dictionary
+        resdic = func(Xcpn, Ycpn, options, *args, **kwargs)  # side effect on options
+        resdic.update({'func_name':options.func_name, 'Xcpn':Xcpn, 'Ycpn':Ycpn, 'Midx':Midx, 'algo_options':vars(options)})
+
+        # Make the output directory if necessary
+        idx = outfile0.rfind(os.path.sep)
+        outdir = outfile0[:idx]
+        try:
+            os.makedirs(outdir)
+        except OSError:
+            pass
+
+        # Save the results in pickle format
+        outfile = outfile0+'.pkl'
+        with open(outfile, 'wb') as fp:
+            pickle.dump(resdic, fp)
+        # # Save the results in json format:
+        # # some non-standard objects might be removed, and non-float values will be casted as float
+        # resjson = to_json(resdic, verbose=options.verbose)
+        # outfile = outfile0+'.json'
+        # with open(outfile, 'w') as fp:
+        #     json.dump(resjson, fp, cls=MyEncoder)
+
+        if options.verbose:
+            print('Results saved in\n{}'.format(outfile))
+
+        return resdic
+    return newfunc
+
+
+# from . import Download_data
+# from . import Preprocess_static
+# from . import Deconv_static
+
+# from .Download_data import Download_data
+# from .Preprocess_static_data import Preprocess_static_data
+# from .Deconv_static_data import Deconv_static_data
+# , Deconv_static_data, Deconv_static_data_plot
